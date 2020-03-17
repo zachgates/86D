376b2f65a3354303ff525ebeb2c5520e00109e6e
Zach Gates <thezachgates@gmail.com>
2020-03-17 18:08:49 -0500

    Use hand selector method

--- play/blackjack/game/BlackjackDealer.py
+++ play/blackjack/game/BlackjackDealer.py
@@ -24,17 +24,17 @@ class BlackjackDealer(CardDealer):
 
     def insurance(self):
         # CardDealer is showing an ace.
-        if self.hands[0].cards[1].rank == 0:
+        if self.hand(0).cards[1].rank == 0:
             # TODO: Ask CardPlayers for insurance bets
             self.log.info('showing an ace; accepting insurance bets')
             # Reveal CardDealer's hand.
-            self.hands[0].up = True
+            self.hand(0).up = True
             # Handle CardDealer blackjack.
-            if self.hands[0].blackjack:
+            if self.hand(0).blackjack:
                 self.log.info('dealer has blackjack')
                 for player in self.table.players[1:]: # Exclude CardDealer.
                     # Reveal CardPlayer's hand.
-                    hand = player.hands[0]
+                    hand = player.hand(0)
                     hand.up = True
                     if hand.blackjack or hand.insured:
                         # CardPlayer receives bets if insured or blackjack.
