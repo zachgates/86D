8fafff9b8b126e9175fdf5994823e3b0f3d93083
Zach Gates <thezachgates@gmail.com>
2020-03-19 23:58:44 -0500

    Subsidized CardPlayers.

--- 86D/__main__.py
+++ 86D/__main__.py
@@ -1,8 +1,8 @@
 if __name__ == '__main__':
     from .core import CardPlayer
     from .play import BlackjackTable
-    p1 = CardPlayer()
-    p2 = CardPlayer()
+    p1 = CardPlayer(_funds=5000)
+    p2 = CardPlayer(_funds=5000)
     t = BlackjackTable()
     t.add_player(p1)
     t.add_player(p2)
